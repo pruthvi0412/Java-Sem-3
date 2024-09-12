86280ad3518f4c7bc580b621d1453cc496b17d3e
pruthvi0412 <pruthvishetty04@gmail.com>
2024-09-12 09:45:51 +0530

    Create K16.java

---
+++ K16.java
@@ -0,0 +1,13 @@
+public class K16{
+    public static void main(String[] args){
+	int upper1=65;
+	int lower1=97;
+	int size=26;
+	for(int i=upper1;i<upper1+size;i++){
+	    System.out.println(i);
+	}
+    }
+}
+//javac -d bin K16.java
+//java -cp bin K16
+
