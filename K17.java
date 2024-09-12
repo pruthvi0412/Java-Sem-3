4a06dda7d6101caebc58971dd82f4ee65d17f501
pruthvi0412 <pruthvishetty04@gmail.com>
2024-09-12 09:47:00 +0530

    Create K17.java

---
+++ K17.java
@@ -0,0 +1,9 @@
+public class K17{
+    public static void main(String[] args){
+	int upper1=65;
+	int size=26;
+	for(int i=upper1;i<upper1+size;i++){
+	    System.out.println((char)i);
+	}
+    }
+}
