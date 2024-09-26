82a32387a4e89988ca17ad7479e96e4658681ad7
pruthvi0412 <pruthvishetty04@gmail.com>
2024-09-26 09:59:42 +0530

    Create K20.java

---
+++ K20.java
@@ -0,0 +1,13 @@
+public class K20{
+    public static void main(String[] args){
+	char ch1, ch2;
+	ch1='A';
+	System.out.println(ch1);
+	ch1++;
+	System.out.println(ch1);
+
+	int n1;
+	n1=ch1;
+	System.out.println(n1);
+    }
+}
