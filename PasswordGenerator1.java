466192e110ffc22abd8decaa2bd4ef40d34d0f04
pruthvi0412 <pruthvishetty04@gmail.com>
2024-10-03 10:49:19 +0530

    Create PasswordGenerator1.java

---
+++ PasswordGenerator1.java
@@ -0,0 +1,17 @@
+*Code for password with 8 characters* 
+
+import java.util.Random;
+
+public class PasswordGenerator {
+    public static void main(String[] args) {
+        String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String lower = "abcdefghijklmnopqrstuvwxyz";
+        String numbers = "0123456789";
+        String all = upper + lower + numbers;
+        String s1 = "";
+        Random random = new Random();
+	int r1 = random.nextInt(26); 
+        s1 = s1 + upper.substring(r1,r1+2)+ lower.substring(r1,r1+4)+ numbers.substring(r1%10,(r1%10)+2);         	
+	System.out.println("Generated Password: " + s1);
+    }
+}
