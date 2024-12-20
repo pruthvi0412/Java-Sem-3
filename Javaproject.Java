8c85eb486b692f7cde38810b63495d367f4f6573
pruthvi0412 <pruthvishetty04@gmail.com>
2024-12-20 13:36:31 +0530

    Create Javaproject.Java

---
+++ Javaproject.Java
@@ -0,0 +1,85 @@
+package exception_divbyzero;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class TestToDoList {
+
+    private static List<String> currentList = new ArrayList<String>();
+    private static Scanner scanner = new Scanner(System.in);  // Declare scanner once
+
+    public static void main(String[] args) {
+
+        int menuItem = -1;
+        while (menuItem != 0) {
+            menuItem = menu();
+            switch (menuItem) {
+                case 1:
+                    showList();
+                    break;
+                case 2:
+                    addItem();
+                    break;
+                case 3:
+                    removeItem();
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Enter a valid option");
+            }
+        }
+    }
+
+    public static int menu() {
+        System.out.println();
+        System.out.println("----------------------");
+        System.out.println("Main Menu");
+        System.out.println("----------------------");
+        System.out.println("0. Exit the program");
+        System.out.println("1. Display to-do list");
+        System.out.println("2. Add item to list");
+        System.out.println("3. Remove item from list");
+        System.out.println();
+        System.out.print("Enter choice: ");
+        int choice = scanner.nextInt();  // Using the class-level scanner
+        return choice;
+    }
+
+    public static void showList() {
+        System.out.println();
+        System.out.println("----------------------");       
+        System.out.println("To-Do List");
+        System.out.println("----------------------");
+        int number = 0;
+        for (String item : currentList) {
+            System.out.println(++number + " " + item);
+        }
+        System.out.println("----------------------");
+    }
+
+    public static void addItem() {
+        System.out.println("Add Item");
+        System.out.println("----------------------");
+        System.out.print("Enter an item: ");
+        scanner.nextLine();  // Clear the buffer
+        String item = scanner.nextLine();  // Read the full line as item
+        currentList.add(item);
+        showList();
+    }
+
+    public static void removeItem() {
+        System.out.println("Remove Item");
+        System.out.println("----------------------");
+        System.out.print("What do you want to remove (Enter the number)? ");
+        int index = scanner.nextInt();
+        if ((index - 1) < 0 || index > currentList.size()) {
+            System.out.println("Wrong index number! Please enter in range of 1 to " + currentList.size());
+        } else {
+            currentList.remove(index - 1);
+        }
+        System.out.println("----------------------");
+        showList();
+    }
+}
